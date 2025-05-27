a845810c4c177df9d00a2258de64c94a4864a4dd
Daniel <fejdidaniel111@gmail.com>
2025-05-27 22:54:17 +0200

    second commit

--- Form1.cs
+++ Form1.cs
@@ -15,24 +15,6 @@ namespace Evidencia_vozidiel
         public Form1()
         {
             InitializeComponent();
-            LoadEvidencija_vozila();
-        }
-
-        private void LoadEvidencija_vozila()
-        {
-            listView1.Items.Clear();
-            using (MySqlConnection conn = new MySqlConnection(conn))
-            {
-                string query = "SELECT VoziloID, Registracija FROM vozilo ORDER BY VoziloID";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                conn.Open();
-                MySqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    listView1.Items.Add(new ListViewItem(reader.GetInt32(0), reader.GetString(1)));
-                }    
-                conn.Close();
-            }
         }
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
